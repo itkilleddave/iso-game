dd531498b24ba4612e14ffb1dbf510ed8ec9eb8c
Dave <Dave@Daves-iMac-2.local>
2015-01-23 19:00:49 +1030

    view2D is rendering Still need to update the sprites to have a neutral 'wall.png', instead of using a directional 'wall_n.png'

--- IsoGame/GameScene.swift
+++ IsoGame/GameScene.swift
@@ -8,38 +8,120 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
+// MARK: Tile
+
+enum Tile: Int {
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+    case Grass
+    case Wall
+    
+    var description:String {
+        switch self {
+        case Grass:
+            return "Grass"
+        case Wall:
+            return "Wall"
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    var image:String {
+        switch self {
+        case Grass:
+            return "grass"
+        case Wall:
+            return "wall_n"
+            
+        }
     }
+    
+    
+}
+
+class GameScene: SKScene {
+    
+    //1
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //2
+    let view2D:SKSpriteNode
+    let viewIso:SKSpriteNode
+    
+    //3
+    let tiles = [
+                [1, 1, 1, 1, 1, 1],
+                [1 ,0, 0, 0, 0, 1],
+                [1 ,0, 0, 0, 0, 1],
+                [1 ,0, 0, 0, 0, 1],
+                [1 ,0, 0, 0, 0, 1],
+                [1, 1, 1, 1, 1, 1]
+                ]
+    let tileSize = (width:32, height:32)
+    
+    //4
+    override init(size: CGSize) {
+        
+        view2D = SKSpriteNode()
+        viewIso = SKSpriteNode()
+        
+        super.init(size: size)
+        self.anchorPoint = CGPoint(x:0.5, y:0.5)
+    }
+
+    
+    //5
+    override func didMoveToView(view: SKView) {
+        
+        view2D.position = CGPoint(x:-self.size.width*0.4, y:self.size.height*0.2)
+        addChild(view2D)
+        
+        viewIso.position = CGPoint(x:self.size.width*0.18, y:self.size.height*0.2)
+        addChild(viewIso)
+        
+        placeAllTiles2D()
+        
+    }
+    
+    func placeTile2D(image:String, withPosition:CGPoint) {
+        
+        let tileSprite = SKSpriteNode(imageNamed: image)
+        
+        tileSprite.position = withPosition
+        
+        view2D.addChild(tileSprite)
+        
+    }
+    
+    func placeAllTiles2D() {
+        
+        //place tiles
+        for i in 0..<tiles.count {
+            
+            let row = tiles[i];
+            
+            for j in 0..<row.count {
+                let tileInt = row[j]
+                
+                //1. assign a new Tile enum, setting it's type via the tileInt value
+                let tile = Tile(rawValue: tileInt)!
+                
+                //2. inverted y value (Sprite Kit coordinate system) we want tiles stacked left to right, then top to bottom
+                var point = CGPoint(x: (j*tileSize.width), y: -(i*tileSize.height))
+                
+                //if this is the hero tile (add an extra tile at this smae position underneath of the default ground tile)
+                /*
+                if (tile.description == "Hero North") {
+                    let tileUnderHero = Tile.Grass
+                    placeTile2D((tileUnderHero.image), withPosition:point)
+                }
+                */
+                
+                placeTile2D(tile.image, withPosition:point)
+            }
+            
+        }
+        
+    }
+    
 }
