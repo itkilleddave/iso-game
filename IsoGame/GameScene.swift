5d8823f1b0c0e7a71581ab7f41f2c991ee62cebf
Dave <Dave@Daves-iMac-2.local>
2015-01-24 22:46:41 +1030

    Added Operator Overloads

--- IsoGame/GameScene.swift
+++ IsoGame/GameScene.swift
@@ -9,6 +9,23 @@
 import UIKit
 import SpriteKit
 
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func * (point: CGPoint, scalar: CGPoint) -> CGPoint {
+    return CGPoint(x: point.x * scalar.x, y: point.y * scalar.y)
+}
+
+func / (point: CGPoint, scalar: CGPoint) -> CGPoint {
+    return CGPoint(x: point.x / scalar.x, y: point.y / scalar.y)
+}
+
 enum Tile: Int {
     
     case Grass
